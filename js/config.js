1420aa5c9095af6467df9faebb550aa0bf481c09
Omer <elomer@gmail.com>
2026-08-14 23:27:32 +0300

    chore: point frontend at deployed production worker

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ export const WFS_URL = 'https://open.govmap.gov.il/geoserver/opendata/ows';
 export const WFS_LAYER = 'opendata:PARCEL_ALL';
 export const PARCEL_IDS_URL = 'data/parcelIds.json';
 export const HOUSES_JSON_URL = 'https://raw.githubusercontent.com/omerel/achi-eilat-street-map/main/houses.json';
-export const WORKER_URL = 'http://127.0.0.1:8787/update-house';
+export const WORKER_URL = 'https://achi-eilat-street-map-worker.achi-eilat-street-map-worker.workers.dev/update-house';
 export const MAP_CENTER = [32.5812, 34.9503];
 export const MAP_ZOOM = 18;
